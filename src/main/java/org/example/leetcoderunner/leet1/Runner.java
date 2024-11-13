dac6962b8789b7330b50447a38df4c01bfb62319
SChrissnyder <schrissnyder@gmail.com>
2024-11-13 13:17:27 -0500

    added in leet runner and Solution for leet code 2563

---
+++ src/main/java/org/example/leetcoderunner/leet1/Runner.java
@@ -0,0 +1,12 @@
+package org.example.leetcoderunner.leet1;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args){
+    Solution a= new Solution();
+
+    int [] nums ={2,7,11,15};
+    System.out.println((Arrays.toString(a.twoSum(nums, 9))));
+    }
+}
