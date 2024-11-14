1654b66df632c3734d8f3a4a1b8c52e04a58e5d5
SChrissnyder <schrissnyder@gmail.com>
2024-11-14 09:38:32 -0500

    added in leet runner and Solution for leet code 20

---
+++ src/main/java/org/example/leetcoderunner/leet20/Runner.java
@@ -0,0 +1,12 @@
+package org.example.leetcoderunner.leet20;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args){
+    Solution a= new Solution();
+
+    String s ="[[]](){}";
+    System.out.println(((a.isValid(s))));
+    }
+}
