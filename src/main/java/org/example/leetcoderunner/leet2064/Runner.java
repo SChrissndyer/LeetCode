af7766353564ee179ec1704d80eab0bca4bb29c1
SChrissnyder <schrissnyder@gmail.com>
2024-11-14 09:53:02 -0500

    added in leet runner and Solution for leet code 2064

---
+++ src/main/java/org/example/leetcoderunner/leet2064/Runner.java
@@ -0,0 +1,12 @@
+package org.example.leetcoderunner.leet2064;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args){
+    Solution a= new Solution();
+
+    int [] nums ={2,7,11,15};
+    System.out.println(((a.minimizedMaximum(5,nums))));
+    }
+}
---
+++ src/main/java/org/example/leetcoderunner/leet2064/Solution.java
@@ -0,0 +1,18 @@
+package org.example.leetcoderunner.leet2064;
+
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int left = 1, right = 100000;
+        while (left < right) {
+            int mid = (left + right) / 2, sum = 0;
+            for (int a : quantities)
+                sum += (a + mid - 1) / mid;
+            if (sum > n)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
+
