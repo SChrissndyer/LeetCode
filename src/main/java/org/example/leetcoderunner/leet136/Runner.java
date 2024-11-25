866785885af96ff6d1b712f011e7e83e6aa00304
SChrissnyder <schrissnyder@gmail.com>
2024-11-25 13:37:50 -0500

    added in for leet code 136 190 191  test

---
+++ src/main/java/org/example/leetcoderunner/leet136/Runner.java
@@ -0,0 +1,10 @@
+package org.example.leetcoderunner.leet136;
+
+public class Runner {
+    public static void main(String[] args){
+    Solution a= new Solution();
+
+    int [] nums ={2,1,1,2,3};
+    System.out.println(a.singleNumber(nums));
+    }
+}
---
+++ src/main/java/org/example/leetcoderunner/leet190/Runner.java
@@ -0,0 +1,10 @@
+package org.example.leetcoderunner.leet190;
+
+public class Runner {
+    public static void main(String[] args){
+    Solution a= new Solution();
+
+    int  nums =50;
+    System.out.println(a.reverseBits(nums));
+    }
+}
